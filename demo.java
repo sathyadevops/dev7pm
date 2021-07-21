231bf1ee5546662b4a255c6dd247ed219c6a4095
sathyadevops <sathyadevops1@gmail.com>
2021-07-21 14:51:00 +0000

    new java code

--- demo.java
+++ demo.java
@@ -1,5 +1,12 @@
 class Demo
 {
+  void getData()
+  {
+    s.o.print(" get Data ");
+    s.o.print(" get Data ");
+    s.o.print(" get Data ");
+    s.o.print(" get Data ");
+  }
   public static void main(...)
   {
     system.out.print(" Hello world ");
